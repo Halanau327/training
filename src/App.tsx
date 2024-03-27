3053457a276a4a4cba9c1ed427ed020664a37f6f
Halanau327 <a.halanau@gmail.com>
2024-03-27 23:28:27 +0300

    training to use usestate hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
 import './App.css';
 import styled from 'styled-components';
-import {NewComponent} from "./components/newComponent";
-import {Button} from "./components/Button";
+import {useState} from "react";
 
 
 function App() {
-    const topCars = [
-        {manufacturer:'BMW', model:'m5cs'},
-        {manufacturer:'Mercedes', model:'e63s'},
-        {manufacturer:'Audi', model:'rs6'}
-    ]
 
-    const Button1Foo = (subscriber: string, age: number, adress: string) => {
-        console.log(subscriber, age, adress)
+    // let a = 1
+    let[a, setA] = useState(1)
+
+    const onclickHandler = () => {
+        setA(++a)
+        console.log(a)
     }
 
-    const Button2Foo = (subscriber: string) => {
-        console.log(subscriber)
+    const onclickHandlerZero = () => {
+        setA(a = 0)
+        console.log(a)
     }
 
-    const Button3Foo = () => {
 
-    }
     return (
         <StyledApp>
-            <NewComponent cars={topCars}/>
-            <Button name={"MyYouTubeChanel-1"} callBack={() => Button1Foo("Vasya", 21, "Minsk")}/>
-            <Button name={"MyYouTubeChanel-2"} callBack={() => Button2Foo("Ivan")}/>
-            <Button name={"Stupid-button"} callBack={() => Button3Foo}/>
+            <h1>{a}</h1>
+            <button onClick={onclickHandler}>number</button>
+            <button onClick={onclickHandlerZero}>0</button>
         </StyledApp>
     )
 }
@@ -37,3 +33,48 @@ export default App;
 const StyledApp = styled.div`
 
 `
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const topCars = [
+//     {manufacturer:'BMW', model:'m5cs'},
+//     {manufacturer:'Mercedes', model:'e63s'},
+//     {manufacturer:'Audi', model:'rs6'}
+// ]
+//
+// const Button1Foo = (subscriber: string, age: number, adress: string) => {
+//     console.log(subscriber, age, adress)
+// }
+//
+// const Button2Foo = (subscriber: string) => {
+//     console.log(subscriber)
+// }
+//
+// const Button3Foo = () => {
+//
+// }
+
+
+{/*<NewComponent cars={topCars}/>*/}
+{/*<Button name={"MyYouTubeChanel-1"} callBack={() => Button1Foo("Vasya", 21, "Minsk")}/>*/}
+{/*<Button name={"MyYouTubeChanel-2"} callBack={() => Button2Foo("Ivan")}/>*/}
+{/*<Button name={"Stupid-button"} callBack={() => Button3Foo}/>*/}
