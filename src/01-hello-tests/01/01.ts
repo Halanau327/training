d148aecb8ea7693cb35883d7154e024c91554246
Halanau327 <a.halanau@gmail.com>
2024-05-14 00:05:26 +0300

    JS/TS native 10 part 2

--- src/01-hello-tests/01/01.ts
+++
@@ -1,7 +0,0 @@
-export const sum = (a: number, b: number) => {
-    return a * b;
-};
-
-export const mult = (a: number, b: number) => {
-    return a + b;
-};
--- src/01-hello-tests/04/04.ts
+++
@@ -1,9 +0,0 @@
-import {CityType, GovernmentBuildingsType} from "../03/03";
-
-export const demolishHousesOnTheStreet = (city: CityType, street: string) => {
-    city.houses = city.houses.filter(h => h.address.street.title !== street)
-}
-
-export const getBuildingsWithStaffCountGreaterThen = (buildings: Array<GovernmentBuildingsType>, number: number) => {
-    return buildings.filter(building => building.staffCount > number)
-}
